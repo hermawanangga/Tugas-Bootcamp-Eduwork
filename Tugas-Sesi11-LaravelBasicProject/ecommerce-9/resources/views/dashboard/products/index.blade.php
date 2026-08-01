dcdd14a79330958141feb2e19873fcb79830c49d
Angga Hermawan <140132767+hermawanangga@users.noreply.github.com>
2026-08-02 04:12:06 +0700

    Tugas Sesi 11
    menambahkan controller untuk mengelola produk dan mengelola halaman

---
+++ Tugas-Sesi11-LaravelBasicProject/ecommerce-9/resources/views/dashboard/products/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('title', 'Contoh halaman product dengan resource controller')
+@section('content')
+  <h1>Contoh halaman product dengan resource controller</h1>
+@endsection
---
+++ Tugas-Sesi11-LaravelBasicProject/ecommerce-9/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('template.layout')
+
+@section('title', 'Home Page')
+
+@section('content')
+<div class="container">
+    <h1>Home Page</h1>
+    <p>Welcome to the home page of the website.</p>
+</div>
+@endsection
